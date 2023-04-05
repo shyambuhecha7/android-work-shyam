c62299e1e196fdf9198bd25d2acf7d7de03240a9
Shyam <shyam.b@simformsolutions.com>
2023-04-05 10:52:43 +0530

    button1 click and change textview with color. button 2 click and acton

---
+++ Feb_17_Android_task/app/src/main/java/com/example/feb_17_android_task_1/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.example.feb_17_android_task_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
