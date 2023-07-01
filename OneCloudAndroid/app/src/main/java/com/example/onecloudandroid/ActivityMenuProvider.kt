80187ec8bc6272ff4f9f0680c28fb7eb214a4276
Shyam <shyam.b@simformsolutions.com>
2023-07-01 18:30:41 +0530

    committed

---
+++ OneCloudAndroid/app/src/main/java/com/example/onecloudandroid/ActivityMenuProvider.kt
@@ -0,0 +1,5 @@
+package com.example.onecloudandroid
+
+interface ActivityMenuProvider {
+    fun getMenu(): Int?
+}
--- OneCloudAndroid/app/src/main/java/com/example/onecloudandroid/HomeActivity.kt
+++ OneCloudAndroid/app/src/main/java/com/example/onecloudandroid/HomeActivity.kt
@@ -2,19 +2,35 @@ package com.example.onecloudandroid
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.example.onecloudandroid.databinding.ActivityHomeBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 class HomeActivity : AppCompatActivity() {
+    lateinit var navController: NavController
+    lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_home)
+
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
-        navView.setupWithNavController(navController)
+        val navigationController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navigationController)
+
+        navigationController.addOnDestinationChangedListener { controller, destination, arguments ->
+            val currentFragment = navHostFragment.childFragmentManager.primaryNavigationFragment
+            currentFragment?.let {
+//                binding.toolbar.menu.clear()
+//                binding.toolbar.inflateMenu(it)
+                binding.tbName.text = destination.label.toString()
+            }
+        }
 
     }
+
+
 }
