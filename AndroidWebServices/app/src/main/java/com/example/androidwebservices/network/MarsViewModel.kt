fb2eacea22dcc9b3a4ec3d3205e2e373a4beabe0
Shyam <shyam.b@simformsolutions.com>
2023-07-10 19:15:00 +0530

    Start Retrofit

--- AndroidWebServices/app/src/main/java/com/example/androidwebservices/network/MarsViewModel.kt
+++ AndroidWebServices/app/src/main/java/com/example/androidwebservices/network/MarsViewModel.kt
@@ -1,10 +1,21 @@
 package com.example.androidwebservices.network
+import android.content.ContentValues.TAG
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.math.log
 
 class MarsViewModel: ViewModel() {
+
+    val
     fun getMarsPhotos() {
-      viewModelScope.launch{}
+      viewModelScope.launch{
+          val listResult = MarsApi.retrofitService.getPhotos()
+
+
+      }
     }
 }
---
+++ AndroidWebServices/app/src/main/java/com/example/androidwebservices/network/NewsModel.kt
@@ -0,0 +1,26 @@
+package com.example.androidwebservices.network
+
+import com.google.gson.annotations.SerializedName
+
+
+data class NewsModel(
+    @SerializedName("status") var status: String,
+    @SerializedName("totalResults") var totalResults: Int,
+    @SerializedName("articles") var articles: List<Articles>
+)
+
+data class Articles(
+    @SerializedName("source") var source: Source,
+    @SerializedName("author") var author: String,
+    @SerializedName("title") var title: String,
+    @SerializedName("description") var description: String,
+    @SerializedName("url") var url: String,
+    @SerializedName("urlToImage") var urlToImage: String,
+    @SerializedName("publishedAt") var publishedAt: String,
+    @SerializedName("content") var content: String
+)
+
+data class Source(
+    @SerializedName("id") var id: String,
+    @SerializedName("name") var name: String
+)
