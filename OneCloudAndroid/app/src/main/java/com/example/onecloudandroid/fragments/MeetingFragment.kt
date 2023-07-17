8e089e58de17e48fc44053a3edac1feecfb1ff57
Shyam <shyam.b@simformsolutions.com>
2023-07-17 19:13:36 +0530

    changes

--- OneCloudAndroid/app/src/main/java/com/example/onecloudandroid/fragments/MeetingFragment.kt
+++ OneCloudAndroid/app/src/main/java/com/example/onecloudandroid/fragments/MeetingFragment.kt
@@ -1,13 +1,17 @@
 package com.example.onecloudandroid.fragments
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
+import android.widget.TimePicker
 import com.example.onecloudandroid.R
 
-class MeetingFragment : Fragment() {
+class MeetingFragment : Fragment(),DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +19,27 @@ class MeetingFragment : Fragment() {
     ): View? {
 
         return inflater.inflate(R.layout.fragment_meeting, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val year: Int = 0
+        val month: Int = 0
+        val day: Int = 0
+        val time: Int = 0
+        val myDay: Int = 0
+        val myMonth: Int = 0
+        val myHour: Int = 0
+        val minute: Int = 0
+
+        val calendarview
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+    }
+
+    override fun onTimeSet(p0: TimePicker?, p1: Int, p2: Int) {
+
     }
 }
